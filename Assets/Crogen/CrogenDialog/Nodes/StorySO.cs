8cb562b3057cbb4a8e43d32b78cb8e29d7614aec
Crogen0407 <mymcoding0407@gmail.com>
2025-07-24 21:43:45 +0900

    feat : Start Node Parameter

--- Assets/Crogen/CrogenDialog/Nodes/StorySO.cs
+++ Assets/Crogen/CrogenDialog/Nodes/StorySO.cs
@@ -5,6 +5,6 @@ namespace Crogen.CrogenDialog
     [CreateAssetMenu(fileName = "StorySO", menuName = "Scriptable Objects/StorySO")]
     public class StorySO : NodeSO
 	{
-    
+        [field:SerializeField] public NodeSO StartNode { get; set; }
     }
 }
