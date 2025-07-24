6a896b44f93770956181d69d8c49b9285c506888
Crogen0407 <mymcoding0407@gmail.com>
2025-07-24 21:41:47 +0900

    feat : NodeSO

---
+++ Assets/Crogen/CrogenDialog/Nodes/NodeSO.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crogen.CrogenDialog
+{
+	// TODO : 나중에 에디터에서 만드는 걸로 바꾸기
+	[CreateAssetMenu(fileName = "NodeSO", menuName = "CrogenDialog/NodeSO")]
+	public abstract class NodeSO : ScriptableObject
+	{
+		[field:SerializeField] public List<NodeSO> InputList { get; private set; }
+		[field:SerializeField] public List<NodeSO> OutputList { get; private set; }
+
+		public void AddInput(NodeSO node)
+		{
+			InputList.Add(node);
+		}
+
+		public void RemoveInput(NodeSO node)
+		{
+			InputList.Remove(node);
+		}
+
+		public void AddOutput(NodeSO node)
+		{
+			OutputList.Add(node);
+		}
+
+		public void RemoveOuput(NodeSO node)
+		{
+			OutputList.Remove(node);
+		}
+	}
+}
---
+++ Assets/Crogen/CrogenDialog/Nodes/StorySO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Crogen.CrogenDialog
+{
+    [CreateAssetMenu(fileName = "StorySO", menuName = "Scriptable Objects/StorySO")]
+    public class StorySO : NodeSO
+	{
+    
+    }
+}
